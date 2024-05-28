fc7dc6e9764f4ba4b9b0287f594b0b6d159f1d23
dh ch <chaalaadhif@gmail.com>
2024-05-28 15:17:36 +0200

    add search-bar

--- src/pages/employeeList/index.jsx
+++ src/pages/employeeList/index.jsx
@@ -1,9 +1,10 @@
 import "./style.css";
 import DataTable from "react-data-table-component";
 import { useSelector } from "react-redux";
-
+import { useState } from "react";
 function EmployeeList() {
      const employees = useSelector((state) => state.employee.employees);
+     const [filterText, setFilterText] = useState("");
 
      const columns = [
           {
@@ -36,13 +37,48 @@ function EmployeeList() {
           { name: "State", selector: (row) => row.state, sortable: true },
           { name: "Zip Code", selector: (row) => row.zipCode, sortable: true },
      ];
-
+     const filteredEmployees = employees.filter(
+          (employee) =>
+               employee.firstname
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.lastname
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.startDate
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.department
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.dateOfBirth
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.street
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.city.toLowerCase().includes(filterText.toLowerCase()) ||
+               employee.state
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+               employee.zipCode.toString().includes(filterText.toLowerCase())
+     );
      return (
           <div className="list-employee-container">
                <h2>Employee List</h2>
+               <div className="search-container">
+                    <input
+                         type="text"
+                         placeholder="Search"
+                         className="search-bar"
+                         value={filterText}
+                         onChange={(e) => setFilterText(e.target.value)}
+                    />
+                    <i className="fa-solid fa-magnifying-glass"></i>
+               </div>
                <DataTable
                     columns={columns}
-                    data={employees}
+                    data={filteredEmployees}
                     pagination
                     highlightOnHover
                     striped
