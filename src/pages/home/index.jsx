195a53984252071fe56564cd4d6851c19510ff95
chaala <chaalaadhif@gmail.com>
2024-08-28 13:37:47 +0200

    delet comments

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,6 @@
 import "./style.css";
-import { Link } from "react-router-dom";
 import Banner from "../../components/banner/index";
-import Card from "../../components/card";
-const features = [
-     {
-          title: "Employee Management",
-          content: "Easily manage all your employee records in one place.",
-     },
-     {
-          title: "Performance Tracking",
-          content: "Track and analyze the performance of your employees.",
-     },
-     {
-          title: "Secure Data",
-          content: "Ensure your employee data is secure and confidential.",
-     },
-];
+
 function Home() {
      return (
           <>
